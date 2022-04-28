ca59f7a12a0d6152be0e94431c930450f868c339
Abeldeboch1 <abeldeboch1@gmail.com>
2022-04-28 11:43:17 -0600

    final product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
 import EmployeeList from './Components/EmployeeList';
 
+
 function App() {
   return (
-    <div className="App">
-         < EmployeeList />
+    <div className="App">           
+         < EmployeeList />        
     </div>
   );
 }
